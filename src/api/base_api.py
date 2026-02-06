0ae9931f0fbb60fba60e7790b5db3fea6bfe0a03
boyna <boyna1212@mail.ru>
2026-02-06 12:08:36 +0300

    Абстрактный класс для работы с API

--- src/api/base_api.py
+++ src/api/base_api.py
@@ -1,13 +1,14 @@
 from abc import ABC, abstractmethod
-from typing import Optional, Dict, Any
+from typing import Any, Dict, Optional
 
 
 class BaseAPI(ABC):
     """Базовый абстрактный класс для всех API клиентов."""
 
     @abstractmethod
-    def _connect_to_api(self, url: str, params: Optional[Dict] = None,
-                        headers: Optional[Dict] = None) -> Optional[Dict[str, Any]]:
+    def _connect_to_api(
+        self, url: str, params: Optional[Dict] = None, headers: Optional[Dict] = None
+    ) -> Optional[Dict[str, Any]]:
         """Приватный метод для подключения к API."""
         pass
 
